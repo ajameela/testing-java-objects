4395234c72e54cb72b41310aee15d9ec2d59e6fe
jahme <jahme@Jummi>
2026-09-10 13:41:42 -0700

    Book Class

--- src/object/Book.java
+++ src/object/Book.java
@@ -2,4 +2,13 @@ package object;
 
 public class Book {
 
+	private String name;
+	private String author;
+	private int year;
+
+	public Book (String name; String author; int year;) {
+		this.name = name;
+		this.author = author;
+		this.year = year;
+	}
 }
